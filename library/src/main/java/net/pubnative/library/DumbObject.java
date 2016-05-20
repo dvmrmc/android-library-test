c51aed18b90fdc386ea85dda8bb655d035da7efb
David Martin <david@martinmacias.com>
2016-05-20 17:51:29 +0200

    added some test and dumb behaviour

---
+++ library/src/main/java/net/pubnative/library/DumbObject.java
@@ -0,0 +1,13 @@
+package net.pubnative.library;
+
+public class DumbObject {
+
+    private String mProperty = null;
+
+    protected void setProperty(String property) {
+        mProperty = property;
+    }
+    protected String getProperty() {
+        return mProperty;
+    }
+}
